9bb3dc1141986a9ee517fefd3ad974b9613ecc68
Joshua Wilding <joshua_wilding@hotmail.com>
2016-04-30 13:35:41 +0800

    ADDED: Logos. ADDED: css to index.

--- server/routes/index.tpl.html.js
+++ server/routes/index.tpl.html.js
@@ -1,9 +1,14 @@
-const template = (params) => `
+const template = (params) => `<!DOCTYPE html>
 <html>
+<head>
+  <link rel="stylesheet" media="all" href="stylesheets/style.css" />
+</head>
 <body>
   <h1>
-    Doctor Gap ${new Date()}
+    Doctor Gap
   </h1>
+  <br>
+  ${new Date()}
 </body>
 </html>
 `
